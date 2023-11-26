bda1e53c1e0c7024e0f0b3551d8dc57eaefb2624
Jack Arthur Harrhy <me@jackharrhy.com>
2023-11-26 12:27:28 -0330

    26/11/2023 tea log, update all packages, fix body css

--- scripts/dither-tealog.fish
+++ scripts/dither-tealog.fish
@@ -1,3 +1,5 @@
+#!/usr/bin/env fish
+
 for file in (find public/assets/tea-log -name tea.jpg)
     set outfile (echo $file | sed 's/\.jpg/\.gif/')
     convert $file -resize 128x128 -dither FloydSteinberg -colors 8 -bordercolor indigo -border 2 $outfile
